8a33147b466effc3bec2090847c082ff1d940d2d
Jeffry Berdugo <jeffryberdugo@freepik.com>
2026-01-29 01:12:19 -0500

    Add Swift-DocC plugin for documentation generation
    - Added swift-docc-plugin dependency to Package.swift
    - This enables 'swift package generate-documentation' command
    - Required for automated DocC generation in CI/CD
    
    Fixes documentation build workflow error.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,9 @@ let package = Package(
             targets: ["SwiftFlow"]
         ),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+    ],
     targets: [
         .target(
             name: "SwiftFlow",
